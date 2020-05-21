a617b636dbb92820d35d3536944e406cbe262aa1
Pietroniro <d.pietroniro@sap.com>
2020-05-21 16:47:33 -0300

    curso 3

---
+++ 003-angular7-componentes-intermediario/src/app/components/alert-success/alert-success.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'alert-success',
+  template: `
+    <div class="alert alert-success" role="alert">
+      <ng-content></ng-content>
+    </div>`
+})
+export class AlertSuccessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ 003-angular7-componentes-intermediario/src/app/components/employee-new-modal/employee-new-modal.component.ts
@@ -0,0 +1,49 @@
+import {Component, ElementRef, OnInit, Output, EventEmitter} from '@angular/core';
+import {Employee, EmployeeService} from '../../services/employee.service';
+
+declare const $;
+
+@Component({
+    selector: 'employee-new-modal',
+    templateUrl: './employee-new-modal.component.html',
+    styleUrls: ['./employee-new-modal.component.css']
+})
+export class EmployeeNewModalComponent implements OnInit {
+
+    employee: Employee = {
+        name: '',
+        salary: 0,
+        bonus: 0,
+    };
+
+    @Output()
+    onSubmit: EventEmitter<Employee> = new EventEmitter<Employee>();
+
+    constructor(private element: ElementRef, private employeeService: EmployeeService) {
+    }
+
+    ngOnInit() {
+    }
+
+    addEmployee(event) {
+        const copy = Object.assign({}, this.employee);
+        this.employeeService.addEmployee(copy);
+        this.onSubmit.emit(copy);
+        this.hide();
+    }
+
+    hide() {
+        const divModal = this.getDivModal();
+        $(divModal).modal('hide');
+    }
+
+    show() {
+        const divModal = this.getDivModal();
+        $(divModal).modal('show');
+    }
+
+    private getDivModal(): HTMLElement {
+        const nativeElement: HTMLElement = this.element.nativeElement;
+        return nativeElement.firstChild.firstChild as HTMLElement;
+    }
+}
